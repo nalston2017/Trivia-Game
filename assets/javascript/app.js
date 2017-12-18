8f9afb1542a92639bc74f9d70ee087ddf5ba3091
Nathan <nathanalston@gmail.com>
2017-12-17 23:56:39 -0500

    Game functional, images added

---
+++ assets/javascript/app.js
@@ -0,0 +1,162 @@
+var triviaQuestions = [{
+  question: "What metal alloy has Wolverine's skeleton been reinforced with?",
+	answerList: ["Iron", "Aluminium", "Adamantium", "Wood"],
+	answer: 2
+},{
+	question: "The first X-men comics was published by Marvel in what year?",
+	answerList: ["1963", "1975", "1965", "1976"],
+	answer: 0
+},{
+	question: "Who wrote the first X-men comic?",
+	answerList: ["Dr. Seuss", "Stan Lee", "Jack Kirby", "Brian Michael Bendis"],
+	answer: 1
+},{
+	question: "Who is Cyclops's son?",
+	answerList: ["Havoc", "Quinton Quire", "Cable", "Colossus"],
+	answer: 2
+},{
+	question: "Who led the New Mutants after Charles Xavier?",
+	answerList: ["Cable", "Magneto", "Pixie", "Moira Mactaggert"],
+	answer: 1
+},{
+	question: "How many X-men movies are there?",
+	answerList: ["10", "8", "9", "7"],
+	answer: 0
+},{
+	question: "Who is the son of Charles Xavier and Moira Mactaggert?",
+	answerList: ["Cyclops", "Brad Bird", "Legion", "Nightcrawler"],
+	answer: 2
+},{
+	question: "Which one of these characters was not on the original X-men team (in the comics)?",
+	answerList: ["Jean Grey", "Storm", "Angel", "Beast"],
+	answer: 1
+},{
+	question: "Who killed Charles Xavier (in the comics)?",
+	answerList: ["Magneto", "Jean Grey", "Captain America", "Cyclops"],
+	answer: 3
+},{
+	question: "How many Wolverines are currently in there currently in Earth 616 in Marvel Comics?",
+	answerList: ["2", "1", "5", "6"],
+	answer: 2
+}];
+
+var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10'];
+var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
+var messages = {
+	correct: "Yes, that's right!",
+	incorrect: "No, that's not it.",
+	endTime: "Out of time!",
+	finished: "Alright! Let's see how well you did."
+}
+
+$('#startBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+$('#startOverBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+function newGame(){
+	$('#finalMessage').empty();
+	$('#correctAnswers').empty();
+	$('#incorrectAnswers').empty();
+	$('#unanswered').empty();
+	currentQuestion = 0;
+	correctAnswer = 0;
+	incorrectAnswer = 0;
+	unanswered = 0;
+	newQuestion();
+}
+
+function newQuestion(){
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+	answered = true;
+
+	//sets up new questions & answerList
+	$('#currentQuestion').html('Question #'+(currentQuestion+1)+'/'+triviaQuestions.length);
+	$('.question').html('<h2>' + triviaQuestions[currentQuestion].question + '</h2>');
+	for(var i = 0; i < 4; i++){
+		var choices = $('<div>');
+		choices.text(triviaQuestions[currentQuestion].answerList[i]);
+		choices.attr({'data-index': i });
+		choices.addClass('thisChoice');
+		$('.answerList').append(choices);
+	}
+	countdown();
+	//clicking an answer will pause the time and setup answerPage
+	$('.thisChoice').on('click',function(){
+		userSelect = $(this).data('index');
+		clearInterval(time);
+		answerPage();
+	});
+}
+
+function countdown(){
+	seconds = 15;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	answered = true;
+	//sets timer to go down
+	time = setInterval(showCountdown, 1000);
+}
+
+function showCountdown(){
+	seconds--;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	if(seconds < 1){
+		clearInterval(time);
+		answered = false;
+		answerPage();
+	}
+}
+
+function answerPage(){
+	$('#currentQuestion').empty();
+	$('.thisChoice').empty(); //Clears question page
+	$('.question').empty();
+
+	var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
+	var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+	$('#gif').html('<img src = "assets/images/'+ gifArray[currentQuestion] +'.gif" width = "400px">');
+  // $('#gif').html('<img src = "assets/images/'+ gifArray[currentQuestion] +'.gif" width = "400px">');
+	//checks to see correct, incorrect, or unanswered
+	if((userSelect == rightAnswerIndex) && (answered == true)){
+		correctAnswer++;
+		$('#message').html(messages.correct);
+	} else if((userSelect != rightAnswerIndex) && (answered == true)){
+		incorrectAnswer++;
+		$('#message').html(messages.incorrect);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+	} else{
+		unanswered++;
+		$('#message').html(messages.endTime);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+		answered = true;
+	}
+
+	if(currentQuestion == (triviaQuestions.length-1)){
+		setTimeout(scoreboard, 5000)
+	} else{
+		currentQuestion++;
+		setTimeout(newQuestion, 5000);
+	}
+}
+
+function scoreboard(){
+	$('#timeLeft').empty();
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+
+	$('#finalMessage').html(messages.finished);
+	$('#correctAnswers').html("Correct Answers: " + correctAnswer);
+	$('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
+	$('#unanswered').html("Unanswered: " + unanswered);
+	$('#startOverBtn').addClass('reset');
+	$('#startOverBtn').show();
+	$('#startOverBtn').html('Start Over?');
+}
